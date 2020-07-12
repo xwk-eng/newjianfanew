834bc3bf4a7944aedb73a62003eb54b91e956bed
xwk-enf <1411681860@qq.com>
2020-07-12 11:08:35 +0800

    huashanjianfa

---
+++ src/main/java/com/atguigu/huashan/jianfa.java
@@ -0,0 +1,14 @@
+package com.atguigu.huashan;
+
+public class jianfa {
+
+    public static void main(String[] args) {
+        System.out.println("dugu9999999999jian");
+        System.out.println("dugu9999999999jian");
+        System.out.println("dugu9999999999jian");
+        System.out.println("dugu9999999999jian");
+
+
+    }
+
+}
